32f564ee180407ccb32407a34d95f54279ad906a
Sam Sharma <samsharma@Sams-MacBook-Pro.local>
2020-04-07 01:40:10 -0700

    line chart set up

--- src/api/index.js
+++ src/api/index.js
@@ -10,4 +10,14 @@ export const fetchData = async () => {
     } catch(error) {
         return error;
     }
-}
+};
+
+export const fetchDailyData = async () => {
+    try {
+      const { data } = await axios.get(`${url}/daily`);
+  
+      return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+    } catch (error) {
+      return error;
+    }
+  };
