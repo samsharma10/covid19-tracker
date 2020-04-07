5e053fbf90927498e26d6c8aba60ecabb79ea72c
Sam Sharma <samsharma@Sams-MacBook-Pro.local>
2020-04-07 00:38:00 -0700

    fetchData from api

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends Component {
+
+  state = {
+    data: {},
+  }
+
+  async componentDidMount(){
+    const fetchData  = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
   render(){
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <Chart />
         <CountryPicker />
       </div>
