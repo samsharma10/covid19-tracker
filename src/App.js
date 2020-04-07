5292c4ba7f199c6362fb09fe0a1677e868440777
Sam Sharma <samsharma@Sams-MacBook-Pro.local>
2020-04-07 00:23:31 -0700

    css module import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
 
 class App extends Component {
   render(){
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards />
+        <Chart />
+        <CountryPicker />
       </div>
     )
     }
